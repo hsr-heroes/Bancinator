a511273753b38e214973e6a582924faee99e67fb
Jonas Wälter <waelter@outlook.de>
2017-03-21 22:28:45 +0100

    Added components Dashboard, Home and Transactions

---
+++ src/app/dashboard/components/index.ts
@@ -0,0 +1,3 @@
+export * from "../dashboard.component";
+export * from "./home.component";
+export * from "./transactions.component";
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,18 +2,22 @@ import {NgModule, ModuleWithProviders} from '@angular/core';
 
 import {SharedModule} from "../shared/shared.module";
 
-import {DashbaordRoutingModule} from "./dashboard-routing.module";
+import {DashboardRoutingModule} from "./dashboard-routing.module";
+import {DashboardComponent} from './dashboard.component';
+import {HomeComponent, TransactionsComponent} from "./components";
 
 @NgModule({
   declarations: [
     // Declarations (Components / Directives) used from/within the Module
+    DashboardComponent, HomeComponent, TransactionsComponent
   ],
   imports: [
     // Other Modules to import (imports the exported Components/Directives from the other module)
-    SharedModule, DashbaordRoutingModule
+    DashboardRoutingModule, SharedModule
   ],
   exports: [
     // Components/Directives (or even Modules) to export (available for other modules; and forRoot() )
+    DashboardComponent, HomeComponent, TransactionsComponent
   ],
   providers: [
     // DI Providers (Services, Tokens, Factories...), may be instantiated multiple times
