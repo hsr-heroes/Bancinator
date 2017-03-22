789273885357204c97abcdd7756ce7ff87edd4e4
Jonas Wälter <waelter@outlook.de>
2017-03-22 15:54:11 +0100

    Added pay/startOver functions

--- src/app/dashboard/components/home.component.ts
+++ src/app/dashboard/components/home.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  private payStatus: Boolean;
+
   constructor() { }
 
   ngOnInit() {
+    this.payStatus = false;
   }
 
+  private pay(event: UIEvent): void {
+    this.payStatus = true;
+  }
+
+  private startOver(event: UIEvent): void {
+    this.payStatus = false;
+  }
 }
