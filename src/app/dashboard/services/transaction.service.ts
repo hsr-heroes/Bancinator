dd1a1b2899de4dbc6375befc98293cb27b5fbf4d
Jonas Wälter <waelter@outlook.de>
2017-03-24 19:48:57 +0100

    Implemented TransactionService

--- src/app/dashboard/services/transaction.service.ts
+++ src/app/dashboard/services/transaction.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
+import {ResourceBase} from '../../auth/resources/resource-base';
+import {Http, Response} from '@angular/http';
+import {Observable} from 'rxjs';
+import {TransactionInfo} from '../models/transaction-info';
 
 @Injectable()
-export class TransactionService {
+export class TransactionService extends ResourceBase{
 
-  constructor() { }
+  constructor(http: Http) {
+    super(http);
+  }
+
+  public getTransactions(count: number): Observable<TransactionInfo> {
+    const url = `/accounts/transactions?count=${count}`;
+    return this.get(url)
+      .map((response: Response) => {
+        const result = response.json();
+        if (result) {
+          return TransactionInfo.fromDto(result);
+        }
+        return null;
+      })
+      .catch((error: any) => {
+        return Observable.of<TransactionInfo>(null);
+      });
+  }
 
 }
