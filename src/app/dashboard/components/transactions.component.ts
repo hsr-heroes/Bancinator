a94e28178f280e3bce099e99d21228b5fb1e8caf
Jonas Wälter <waelter@outlook.de>
2017-03-25 10:27:41 +0100

    Implemented Transaction component

--- src/app/dashboard/components/transactions.component.ts
+++ src/app/dashboard/components/transactions.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Transaction} from '../models/transaction';
+import {TransactionInfo} from '../models/transaction-info';
+import {TransactionService} from '../services/transaction.service';
 
 @Component({
   selector: 'app-transactions',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionsComponent implements OnInit {
 
-  constructor() { }
+  private years: number [];
+  private selectedYear: number;
+  private months: Date [];
+  private selectedMonth: number;
+  private transactions: Transaction [];
+
+  constructor(private transSvc: TransactionService) { }
 
   ngOnInit() {
+    this.months = Array.apply(null, {length: 12}).map(Function.call, Number).map( (x) => new Date().setMonth(x));
+    this.selectedMonth = new Date().getMonth();
+    const year = new Date().getFullYear();
+    this.years = [year - 2, year - 1, year];
+    this.selectedYear = year;
+    this.getTransactions();
+  }
+
+  private getTransactions(): void {
+    this.transSvc.getTransactions(null, this.selectedYear, this.selectedMonth).subscribe(
+      (data: TransactionInfo) => {
+        this.transactions = data.result;
+      }
+    );
   }
 
 }
